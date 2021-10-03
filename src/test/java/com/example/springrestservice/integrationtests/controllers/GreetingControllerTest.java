4f749cea68b14aed894f094ecd419544731d402b
Jay Bisa <jay.bisa@gmail.com>
2021-10-03 16:46:56 -0400

    Add unit and integration tests.

---
+++ src/test/java/com/example/springrestservice/integrationtests/controllers/GreetingControllerTest.java
@@ -0,0 +1,61 @@
+package com.example.springrestservice.integrationtests.controllers;
+
+import com.example.springrestservice.records.Greeting;
+import com.example.springrestservice.unittests.controllers.GreetingController;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(GreetingController.class)
+class GreetingControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    void greetingWithoutRequestParam() throws Exception {
+        // Arrange
+        RequestBuilder request = MockMvcRequestBuilders.get("/greeting");
+        Greeting expected = new Greeting("Hello, World!");
+
+        // Act
+        String json = mvc
+                .perform(request)
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        Greeting result = new ObjectMapper().readValue(json, Greeting.class);
+
+        // Assert
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void greetingWithRequestParam() throws Exception {
+        // Arrange
+        RequestBuilder request = MockMvcRequestBuilders.get("/greeting?name=Niam");
+        Greeting expected = new Greeting("Hello, Niam!");
+
+        // Act
+        String json = mvc
+                .perform(request)
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        Greeting result = new ObjectMapper().readValue(json, Greeting.class);
+
+        // Assert
+        assertEquals(expected, result);
+    }
+}
---
+++ src/test/java/com/example/springrestservice/unittests/controllers/GreetingControllerTest.java
@@ -0,0 +1,22 @@
+package com.example.springrestservice.unittests.controllers;
+
+import com.example.springrestservice.records.Greeting;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class GreetingControllerTest {
+
+    @Test
+    void TestGreeting() {
+        // Arrange
+        GreetingController controller = new GreetingController();
+        Greeting expected = new Greeting("Hello, Test!");
+
+        // Act
+        Greeting result = controller.greeting("Test");
+
+        // Assert
+        assertEquals(expected, result);
+    }
+}
