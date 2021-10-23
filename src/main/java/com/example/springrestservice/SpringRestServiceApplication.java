c50d9bf807480e034e0e28824236a5c63b2a0fe5
Jay Bisa <jay.bisa@gmail.com>
2021-10-23 17:26:15 -0400

    Remove unused imports.

--- src/main/java/com/example/springrestservice/SpringRestServiceApplication.java
+++ src/main/java/com/example/springrestservice/SpringRestServiceApplication.java
@@ -2,9 +2,6 @@ package com.example.springrestservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class SpringRestServiceApplication {
